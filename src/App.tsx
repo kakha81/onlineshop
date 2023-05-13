6f3c0f1e592060341160e91f511ffa6e56c9f80d
Kakhaber Mchedlishvili <kakha81@gmail.com>
2023-05-13 20:21:41 +0400

    add some correction

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Home from './pages/Home';
@@ -9,15 +9,17 @@ import Profile from './pages/Profile';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/home' element={<Home />} />
-      <Route path='/productlist' element={<ProductList />} />
-      <Route path='/product' element={<Product />} />
-      <Route path='/cart' element={<Cart />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/profile' element={<Profile />} />
-    </Routes>
+    <Router basename='/onlineshop'>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/productlist' element={<ProductList />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </Router>
   );
 };
 
