01ed73f7601a807d5c14f197dd839d887835af94
neomaster <kakha81@gmail.com>
2023-05-23 14:54:30 +0400

    add setTimeout

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 import { ProductsArray } from "../data";
-
 const SliderContainer = styled.section`
   position: relative;
-  
 `;
 
 const LeftArrow = styled(FaArrowAltCircleLeft)`
@@ -14,9 +12,12 @@ const LeftArrow = styled(FaArrowAltCircleLeft)`
   left: 20px;
   font-size: 2em;
   color: teal;
-  opacity: 0.8;
   transform: translateY(-50%);
   cursor: pointer;
+  transition: 0.1s all ease-in;
+  &:active {
+    color: red;
+  }
 `;
 
 const RightArrow = styled(FaArrowAltCircleRight)`
@@ -25,38 +26,42 @@ const RightArrow = styled(FaArrowAltCircleRight)`
   right: 20px;
   font-size: 2em;
   color: teal;
-  opacity: 0.8;
   transform: translateY(-50%);
   cursor: pointer;
+  transition: 0.1s all ease-in;
+  &:active {
+    color: red;
+  }
 `;
 
 const ImageContainer = styled.div`
-display: flex;
-justify-content: center;
+  display: flex;
+  justify-content: center;
 `;
 
 const Image = styled.img`
-  width: 50%;
   height: 300px;
   object-fit: contain;
-  
 `;
 
 const Slider = () => {
   const [current, setCurrent] = useState(0);
-  const length = ProductsArray.length;
+  const slideCount = ProductsArray.length;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCurrent(current => (current === slideCount - 1 ? 0 : current + 1));
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [current]);
 
   const prevSlide = () => {
-    setCurrent(current => (current === 0 ? length - 1 : current - 1));
+    setCurrent(current => (current === 0 ? slideCount - 1 : current - 1));
   };
 
   const nextSlide = () => {
-    setCurrent(current => (current === length - 1 ? 0 : current + 1));
+    setCurrent(current => (current === slideCount - 1 ? 0 : current + 1));
   };
-
-  if (!Array.isArray(ProductsArray) || ProductsArray.length === 0) {
-    return null;
-  }
 
   return (
     <SliderContainer>
@@ -64,9 +69,7 @@ const Slider = () => {
       <RightArrow onClick={nextSlide} />
       {ProductsArray.map((slide, index) => (
         <ImageContainer key={index}>
-          {index === current && (
-            <Image src={slide.img} alt="" />
-          )}
+          {index === current && <Image src={slide.img} alt="" />}
         </ImageContainer>
       ))}
     </SliderContainer>
