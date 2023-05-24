e571383b94d249a1bd094941cf30d4366d869b4b
neomaster <kakha81@gmail.com>
2023-05-24 17:25:26 +0400

    edit index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
@@ -9,17 +9,15 @@ import Profile from "./pages/Profile";
 
 const App = () => {
   return (
-    <Router basename="/onlineshop">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/productlist" element={<ProductList />} />
-        <Route path="/product/:itemId" element={<ProductPage />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/productlist" element={<ProductList />} />
+      <Route path="/product/:itemId" element={<ProductPage />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
   );
 };
 
