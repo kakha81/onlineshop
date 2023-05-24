973dc267c441cea221faf6e43d963c3f7b75fed6
neomaster <kakha81@gmail.com>
2023-05-24 13:56:48 +0400

    edit url

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Profile from "./pages/Profile";
 
 const App = () => {
   return (
-    <Router basename="/onlineshop">
+    <Router basename="/">
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
